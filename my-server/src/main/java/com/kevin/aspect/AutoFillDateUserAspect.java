49cfa3bf6e0eaf9be40139f2706a6c62ff0d620b
kevin <kevinxuzhimeng@gmail.com>
2025-10-10 01:57:35 +1100

    rename aspect to autofilldateuser

--- my-server/src/main/java/com/kevin/aspect/AutoFillDateUserAspect.java
+++ my-server/src/main/java/com/kevin/aspect/AutoFillDateUserAspect.java
@@ -1,5 +1,6 @@
 package com.kevin.aspect;
 
+import com.kevin.Constant.AutoFillConstant;
 import com.kevin.Enum.UpdateEnum;
 import com.kevin.context.BaseContext;
 import org.aspectj.lang.JoinPoint;
@@ -13,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 
@@ -40,35 +42,40 @@ public class AutoFillDateUserAspect {
 
 
     @Before("@annotation(com.kevin.aspect.AutoFillDateUser)")
-    public void beforeAddDate(JoinPoint joinPoint) throws Exception {
-        //拿到方法和参数的相关信息
-        log.info(joinPoint.getTarget().getClass().getName());
-        log.info(joinPoint.getSignature().toString());
+    public void beforeAddDate(JoinPoint joinPoint)  {
+        //拿到方法的参数
         Object[] args = joinPoint.getArgs();
 
-        int i = Integer.MIN_VALUE;
+        if (args[0] == null) {
+            return;
+        }
 
-        // 如何取到“update”
-
+        // 拿到方法的方法对象（通过signature），然后拿到方法的注解
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        AutoFillDateUser addDate = method.getAnnotation(AutoFillDateUser.class);
-        UpdateEnum value = addDate.value();
+        Method method = methodSignature.getMethod(); //获得方法对象 //getName是获得方法名
+        AutoFillDateUser autoFillDateUser = method.getAnnotation(AutoFillDateUser.class);//获取指定的注解
+        UpdateEnum value = autoFillDateUser.value();
+
 
         Method setUpdateTime;
         Method setUpdateUser;
 
-        setUpdateTime = args[0].getClass().getDeclaredMethod("setUpdateTime", LocalDateTime.class);
-        setUpdateTime.invoke(args[0], LocalDateTime.now());
+        try {
+            setUpdateTime = args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+            setUpdateTime.invoke(args[0], LocalDateTime.now());
 
-        setUpdateUser = args[0].getClass().getDeclaredMethod("setUpdateUser", Long.class);
-        setUpdateUser.invoke(args[0], BaseContext.getTokenId());
+            setUpdateUser = args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+            setUpdateUser.invoke(args[0], BaseContext.getTokenId());
 
-        if (value == UpdateEnum.ADD) {
-            Method setCreateTime = args[0].getClass().getDeclaredMethod("setCreateTime", LocalDateTime.class);
-            setCreateTime.invoke(args[0], LocalDateTime.now());
-            Method setCreateUser = args[0].getClass().getDeclaredMethod("setCreateUser", Long.class);
-            setCreateUser.invoke(args[0], BaseContext.getTokenId());
+            //getClass() 方法参数对象
+            if (value == UpdateEnum.ADD) {
+                Method setCreateTime = args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+                setCreateTime.invoke(args[0], LocalDateTime.now());
+                Method setCreateUser = args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+                setCreateUser.invoke(args[0], BaseContext.getTokenId());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         System.out.println("after auto fill date user aspect" + args[0]);
